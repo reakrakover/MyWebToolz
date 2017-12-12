ce785bdbe513c578b9a5845fa86374697a3d87bd
Tamarrothenberg <tamarr@safeark.co.uk>
2017-12-12 23:56:21 +0200

    First Commit

--- home_page/static/home_page/bundle.js
+++ home_page/static/home_page/bundle.js
@@ -3,6 +3,7 @@ function r(f){/in/.test(document.readyState)?setTimeout('r('+f+')',9):f()}
 r(function(){
   var button = document.getElementById("button");
   button.disabled = true;
+
 });
 
 function checkLength() {
@@ -17,8 +18,33 @@ function checkLength() {
 }
 
 function formSubmit() {
+  var text = $("#textArea").val();
+  var lines = text.split(/\r|\r\n|\n/);
+  var count = lines.length;
+
   button.disabled = true;
   button.style.cursor = "wait";
-  
-  
+
+  document.getElementById('form').style.display = "none";
+  document.getElementById('description').style.display = "none";
+  document.getElementById('ytvid').style.display = "flex";
+  document.getElementById('player').style.display = "block";
+  document.getElementById('loadingGif').style.display = "block";
+  document.getElementById('massage').innerHTML = "<span>" + count + " url's?! that could take a while...<br> so please enjoy this lovely video while we getting all your images size</span>";
+
+  var videos = [
+    "FNQxxpM1yOs",
+    "8UVNT4wvIGY",
+    "FavUpD_IjVY",
+    "jdYJf_ybyVo",
+    "23QOy9Q2qNI",
+    "PDhVJfEFxik",
+    "CF5u78G7pY4",
+    "nfWlot6h_JM",
+    "VHtKx2jk40U"
+  ];
+  var randomItem = videos[Math.random() * videos.length | 0];
+
+  document.getElementById('player').setAttribute("src", "http://www.youtube.com/embed/" + randomItem + "?autoplay=1");
+
 }
--- images_size/views.py
+++ images_size/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 import requests
-import json as simplejson
 from datetime import datetime
 
 
 def results(request):
     images_split = {}
     lines_count = []
+    number_of_images = len(lines_count)
     start_time = datetime.now()
 
     if request.method == 'POST':
@@ -20,16 +20,16 @@ def results(request):
             error = 'Enter at least 1 image URL'
             return render(request, 'images_size/results.html', {'error': error})
 
-        elif len(lines_count) > 11:
+        elif number_of_images > 11:
             error = 'No More then 10 files'
-            js_data = simplejson.dumps(error)
-            return render(request, 'images_size/results.html', {'error': error,
-                                                                'js_data': js_data})
+            return render(request, 'images_size/results.html', {'error': error})
 
         else:
             for line in image.split('\r'):
                 line = line.replace('\n', '')
-                if line[:4] != 'http':
+                if line == '':
+                    images_split[line] = 'EMPTY URL'
+                elif line[:4] != 'http':
                     images_split[line] = 'URL ERROR'
                 else:
                     try:
@@ -59,7 +59,7 @@ def results(request):
                             return render(request, 'images_size/results.html', {'error': error})
 
             runtime = datetime.now() - start_time
-            return render(request, 'images_size/results.html', {
+            return render(request, 'images_size/results.html', {'number_of_images': number_of_images,
                                                                 'images_split': images_split,
                                                                 'pick': pick,
                                                                 'runtime': runtime
